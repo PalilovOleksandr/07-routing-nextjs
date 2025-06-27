a1e2f611f499d02453c000b89e64a580b5c20690
Oleksandr Palilov <opalilovit@gmail.com>
2025-06-28 00:29:24 +0300

    edit app/page.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,18 @@
-import type { CSSProperties } from "react";
-import { ScaleLoader } from "react-spinners";
+import type { CSSProperties } from 'react';
+import { ScaleLoader } from 'react-spinners';
+import css from './page.module.css';
 
 const override: CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
+  display: 'flex',
+  justifyContent: 'center',
 };
 
 const Loader = () => {
-    return (
-        <ScaleLoader color="#dc3545" cssOverride={override} />
-    );
+  return (
+    <div className={css.backdrop}>
+      <ScaleLoader color="#dc3545" cssOverride={override} />
+    </div>
+  );
 };
 
 export default Loader;
